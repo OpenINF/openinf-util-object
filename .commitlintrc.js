690a2c32d580f16d384beda79474e7042d0cdfe6
Derek Lewis <DerekNonGeneric@inf.is>
2022-08-06 04:21:06 -0400

    chore(conf): add new `style` commit type
    Signed-off-by: Derek Lewis <DerekNonGeneric@inf.is>

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,7 +3,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'deps', 'chore']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'deps', 'chore', 'style']],
     'header-max-length': [2, 'always', 50],
     'subject-case': [0, 'always', ['lower-case', 'sentence-case', 'start-case']],
   },
