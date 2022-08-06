d92bdc83e65ec31083bf3f045cd66f9052ef9655
Derek Lewis <DerekNonGeneric@inf.is>
2022-08-06 03:16:41 -0400

    fix(conf): commit summaries to contain < 50 chars
    ProTip! Place extra information in the extended description.
    
    Signed-off-by: Derek Lewis <DerekNonGeneric@inf.is>

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'deps', 'chore']],
-    'header-max-length': [2, 'always', 80],
+    'header-max-length': [2, 'always', 50],
     'subject-case': [0, 'always', ['lower-case', 'sentence-case', 'start-case']],
   },
 }
