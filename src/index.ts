326d1b6387edf0837e171edd2eae4d84962e9afa
Brian Greig <brian@resurgencewebdesign.com>
2022-10-22 22:44:46 -0400

    bug fix on boolean

--- src/index.ts
+++ src/index.ts
@@ -135,7 +135,7 @@ export const deepMerge = (
 export const objectsEqualShallow = (
   o1: Record<string, number | RegExp> | null | undefined,
   o2: Record<string, number | RegExp> | null | undefined
-): boolean {
+): boolean => {
   if (o1 == undefined || o2 == undefined) {
     // Null is only equal to null, and undefined to undefined.
     return o1 === o2;
